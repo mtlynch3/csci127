1fa78ccfb509f0e83dfc95a73bb9684560dd8d1c
Melissa Lynch <lynch.melissat@gmail.com>
2024-11-05 08:03:57 -0500

    Update num2string.py

--- lecture09/num2string.py
+++ lecture09/num2string.py
@@ -1,5 +1,29 @@
-ls = ["zero", "one", "two"]
+#LECTURE SLIP PROBLEM 1
 
-num = 2
+def prob4(amy, beth):
+  if amy > 4:
+    print("Easy case")
+    kate = -1
+  else:
+    print("Complex case")
+    kate = helper(amy, beth)
+  return (kate)
 
-print(ls[num])
+def helper(meg, jo):
+  s = ""
+  for j in range(meg):
+    print(j, ":", jo[j])
+    if j % 2 == 0:
+      s = s + jo[j]
+      print("Building s:", s)
+  return s
+
+r = prob4(4, "city")
+print("Return r: ", r)
+r = prob4(2, "university")
+print("Return r: ", r)
+
+#LECTURE SLIP PROBlEM 2
+def num2string(num):
+  numString = ""
+  return numString
